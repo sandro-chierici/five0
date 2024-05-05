27ae3f26d9e351fac26516707a03307b2a10722a
sandro chierici <sandro.chierici@gmail.com>
2024-05-05 22:47:24 +0200

    Modifying rabbit client

--- src/services/RabbitKafkaBridge/cmd/rkbridge/main.go
+++ src/services/RabbitKafkaBridge/cmd/rkbridge/main.go
@@ -8,38 +8,57 @@ import (
 	"syscall"
 )
 
+// Program management
+type PM struct {
+	done chan bool
+	sig  chan os.Signal
+	waitingGroup 
+}
+
+func NewPM() *PM {
+
+	var pm = &PM{
+		done: make(chan bool, 1),
+		sig:  make(chan os.Signal, 10),
+		wai
+	}
+
+	signal.Notify(pm.sig, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		// waitin for kill signal
+		<-pm.sig
+		// signal all to close 
+		pm.done <- true
+
+	}()
+
+	return pm
+}
+
 func main() {
 
 	parseFlags()
 
-	// handling stops
-	sig := make(chan os.Signal, 10)
-	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-sig
-		fmt.Println("\nReceived signal, program exit. Bye")
-		os.Exit(0)
-	}()
+	var pm = NewPM()
 
 	fmt.Println("Starting")
 
 	// start connections
 	go connectRabbit()
 
-	done := make(chan bool, 1)
-	<-done
+	<-pm.done
 }
 
 // parseFlags parses the flags passed on reexec to create the TCP/UDP/SCTP
 // net.Addrs to map the host and container ports.
-func parseFlags() (topic string, rabbitQueue string) {
-	var (
-		tp = flag.String("topic", "five0.signals", "kafka topic to write into")
-		rq = flag.String("queue", "five0.signals", "rabbitmq queue to read from")
-	)
+func parseFlags() (tenant string) {
+
+	var tenantId = *flag.String("tenant", "five0_test", "rabbitmq tenant name")
+
 	flag.Parse()
-	fmt.Printf("program settings - topic: %s, queue: %s\n", *tp, *rq)
-	return *tp, *rq
+	fmt.Printf("program settings - rabbitmq tenant name: %s\n", tenantId)
+	return tenantId
 }
 
 func connectRabbit() {
